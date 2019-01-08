0f19728b9f459e6e3b1d0d4f517725fe1d7c5bd0
kaitoland <kaitoland1011@gmail.com>
2019-01-09 03:13:34 +0900

    my first commit

---
+++ TODOappli/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TODOappli.Models;
+
+namespace TODOappli.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
